07c3e3e2bc9f577fdb2c0d8a59a4c4ace9324367
petrkucerak <petr@khome.cz>
2024-04-04 06:22:48 +0200

    Update script to create zip file for brute

--- hw/hw04/toBrute.sh
+++ hw/hw04/toBrute.sh
@@ -1,3 +1,4 @@
 cp src/test/java/cz/cvut/fel/esw/matrixmultiplication/MatrixMultiplicationBenchmark.java MatrixMultiplicationBenchmark.java
+cp assets/report/benchmark_template.pdf report.pdf
 zip -r kucerp28.zip results.json measurements.json report.pdf MatrixMultiplicationBenchmark.java
 rm MatrixMultiplicationBenchmark.java
